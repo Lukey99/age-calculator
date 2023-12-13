42c0c6bde36359a6db126e711de1d8d356ffce4a
Lukey99 <kenguyen@et.esiea.fr>
2023-12-13 18:26:10 +0100

    installing fonts

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Hello</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style lang="scss">
+@font-face {
+  font-family: "Poppins";
+  src: url("./assets/fonts/poppins-regular-webfont.woff2") format("woff2"),
+    url("./assets/fonts/poppins-regular-webfont.woff") format("woff");
+  font-weight: normal;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "Poppins-Bold";
+  src: url("./assets/fonts/poppins-bold-webfont.woff2") format("woff2"),
+    url("./assets/fonts/poppins-bold-webfont.woff") format("woff");
+  font-weight: bold;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "Poppins-BoldItalic";
+  src: url("./assets/fonts/poppins-bolditalic-webfont.woff2") format("woff2"),
+    url("./assets/fonts/poppins-bolditalic-webfont.woff") format("woff");
+  font-weight: bold;
+  font-style: italic;
+}
+
+@font-face {
+  font-family: "Poppins-ExtraBoldItalic";
+  src: url("./assets/fonts/poppins-extrabolditalic-webfont.woff2") format("woff2"),
+    url("./assets/fonts/poppins-extrabolditalic-webfont.woff") format("woff");
+  font-weight: bolder;
+  font-style: italic;
+}
+
+/* Utilisez votre police dans d'autres sélecteurs CSS */
+body {
+  font-family: "Poppins-ExtraBoldItalic", sans-serif;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  left: 0;
+  top: 0;
+}
+
+:root {
+  --white: hsl(0, 0%, 100%);
+  --off-white: hsl(0, 0%, 94%);
+  --light-grey: hsl(0, 0%, 86%);
+  --smockey-grey: hsl(0, 1%, 44%);
+  --off-black: hsl(0, 0%, 8%);
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100vw;
+  height: 100vh;
+  background: var(--off-white);
 }
 </style>
